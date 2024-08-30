e32c00930d56f7b12444af75ced1eebddf6d39e7
George Lam <georgelamd@gmail.com>
2024-08-30 17:43:26 -0400

    Fix indentation

--- src/Block.cpp
+++ src/Block.cpp
@@ -4,12 +4,12 @@
 #include "../include/HashUtil.h"
 
 Block::Block(const std::string &prevHash, const std::string &data) : prevHash(prevHash), data(data) {
-const auto now = std::chrono::system_clock::now();
-const auto sinceEpoch = now.time_since_epoch();
-timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(sinceEpoch).count();
+    const auto now = std::chrono::system_clock::now();
+    const auto sinceEpoch = now.time_since_epoch();
+    timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(sinceEpoch).count();
 
-std::string input = prevHash + data + std::to_string(timestamp);
-hash = HashUtil::hash(input);
+    std::string input = prevHash + data + std::to_string(timestamp);
+    hash = HashUtil::hash(input);
 }
 
 std::string Block::getHash() const {
